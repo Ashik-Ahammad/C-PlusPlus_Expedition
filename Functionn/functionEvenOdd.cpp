656aa145d33c47a641c84b75e9150259a2bdd86d
Ashik-Ahammad <ashik19ahammad@gmail.com>
2023-06-09 07:14:00 +0600

    5.0

---
+++ Functionn/functionEvenOdd.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+bool isEven(int a){
+
+    if(a&1){
+        //odd
+        return 0;
+    }
+    else{
+        //even
+        return 1;
+    }
+
+    
+}
+
+int main(){
+
+    int num;
+    cin>>num;
+
+    
+    if (isEven(num)){
+        cout<<"Number is even."<<endl;
+    }
+    else{
+        cout<<"Number is odd."<<endl;
+    }
+    
+
+
+    return 0;
+}
