1cedc358606a11abff839f29d9737359161a73d1
Ashik-Ahammad <ashik19ahammad@gmail.com>
2023-07-19 05:58:25 +0600

    7.0.0.1

---
+++ Arrays/findDuplicates.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+
+    sort(nums.begin(), nums.end());
+
+    int ans;
+
+    for(int i=0; i<nums.size(); i++){
+
+            if(nums[i] == nums[i+1]){
+                ans = nums[i];
+                break;
+            }
+
+    }
+        return ans;
+}
+    
+};
---
+++ Arrays/findUnique.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int findUnique(int arr, int size){
+
+    int ans = 0;
+
+    for(int i=0; i<size; i++){
+        ans = ans^arr[i];
+    }
+    return ans;
+}
+
+int main(){
+
+    findUnique(1, 7);
+}
+
